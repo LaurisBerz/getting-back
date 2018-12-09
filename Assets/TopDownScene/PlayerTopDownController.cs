0f1ff000904289bcd82fb49d049b964892066c6c
LaurisBerz <laurisberz15@gmail.com>
2018-12-09 21:57:00 +0200

    Added shooting, remade movement.

--- Assets/TopDownScene/PlayerTopDownController.cs
+++ Assets/TopDownScene/PlayerTopDownController.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class PlayerTopDownController : MonoBehaviour {
 
-    public float playerSpeed = 8f;
+    public float playerSpeed = 5f;
 
     private Rigidbody playerRigidbody;
-    private Vector3 movement;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
 
-	void Start () {
+    private Camera mainCamera;
+
+    public GunController theGun;
+
+    void Start () {
         playerRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
 	}
-	
-	void Update () {
-        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * playerSpeed * Time.deltaTime;
 
-        playerRigidbody.transform.Translate(movement);
-        Debug.Log(movement);
-	}
+    void Update() {
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * playerSpeed;
+        moveVelocity = moveInput * playerSpeed;
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength)) {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.green);
+
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+
+        if (Input.GetMouseButtonDown(0)) {
+            theGun.isFiring = true;
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            theGun.isFiring = false;
+        }
+    }
+
+    void FixedUpdate () {
+        playerRigidbody.velocity = moveVelocity;
+    }
+
 }
